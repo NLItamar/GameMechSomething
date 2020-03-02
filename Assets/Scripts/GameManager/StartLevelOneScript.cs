e30a5c281f882d948d620b5a6fe08beed617d723
Itamar <itamar@live.nl>
2020-03-02 15:03:15 +0100

    start text and it fades out

--- Assets/Scripts/GameManager/StartLevelOneScript.cs
+++ Assets/Scripts/GameManager/StartLevelOneScript.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartLevelOneScript : MonoBehaviour
 {
     public GameObject RoofObject;
+    public Text StartText;
 
     // Start is called before the first frame update
     void Start()
     {
+        //setting the roof active
         RoofObject.SetActive(true);
+
+        StartText.GetComponent<TextFadeOut>().FadeOut();
     }
 }
