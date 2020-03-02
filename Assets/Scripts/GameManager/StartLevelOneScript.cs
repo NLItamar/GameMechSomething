ab9af59ad8bcd5aa729e6b2de6839fd061dc70f6
Itamar <itamar@live.nl>
2020-03-02 14:27:55 +0100

    moving some scripts and having a roof. roof appears when the game starts

---
+++ Assets/Scripts/GameManager/StartLevelOneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevelOneScript : MonoBehaviour
+{
+    public GameObject RoofObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RoofObject.SetActive(true);
+    }
+}
