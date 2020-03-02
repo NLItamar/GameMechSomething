d266be9c7b60a278b5fcae23b4d38aaf7441c4ba
Itamar <itamar@live.nl>
2020-03-02 16:45:35 +0100

    torchcontroller setup, on and off and no other stuffs yet

---
+++ Assets/Scripts/GameManager/TorchController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    public bool IsTorchOn;
+    public Light Torch;
+
+    void Start()
+    {
+        IsTorchOn = false;
+        Torch.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            TorchUse();
+        }
+    }
+
+    void TorchUse()
+    {
+        if(IsTorchOn == false && Torch.gameObject.activeSelf == false)
+        {
+            IsTorchOn = true;
+            Torch.gameObject.SetActive(true);
+        }
+        else if(IsTorchOn == true && Torch.gameObject.activeSelf == true)
+        {
+            IsTorchOn = false;
+            Torch.gameObject.SetActive(false);
+        }
+    }
+}
