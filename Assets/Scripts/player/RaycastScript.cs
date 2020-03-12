d595c877ebfcdd329267e06715eef2494ac53129
Itamar <itamar@live.nl>
2020-03-12 18:28:47 +0100

    materials for the walls and ground. crosshair that goes another colour when an interactable is in raycast. starting for highlighting object on raycast

---
+++ Assets/Scripts/player/RaycastScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RaycastScript : MonoBehaviour
+{
+    private GameObject raycastedObject;
+
+    [SerializeField] private int rayLength = 4;
+    [SerializeField] private LayerMask layerMaskInteract;
+
+    [SerializeField] private Image UICrosshair;
+
+    //private bool raycastHit = false;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if(Physics.Raycast(transform.position, fwd, out hit, rayLength, layerMaskInteract.value))
+        {
+            if(hit.collider.CompareTag("InteractableObject"))
+            {
+                raycastedObject = hit.collider.gameObject;
+                //change the crosshair
+                CrosshairActive();
+
+                if(Input.GetKeyDown("e"))
+                {
+                    Debug.Log("I have interacted");
+                    raycastedObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            //crosshair to normal
+            CrosshairNormal();
+        }
+    }
+
+    void CrosshairActive()
+    {
+        UICrosshair.color = Color.green;
+    }
+
+    void CrosshairNormal()
+    {
+        UICrosshair.color = Color.white;
+    }
+}
