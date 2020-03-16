b179214e81102d13b9510d3ea71b959f0dfb6922
Itamar <itamar@live.nl>
2020-03-16 13:14:30 +0100

    simple interaction going, text displayed on the objects.

---
+++ Assets/Scripts/GameManager/JournalProgression.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalProgression : MonoBehaviour
+{
+    public Text journalText;
+
+    int caseSwitch;
+
+    private void Start()
+    {
+        caseSwitch = 0;
+    }
+
+    public void JournalAddText()
+    {
+        caseSwitch++;
+
+        switch (caseSwitch)
+        {
+            case 1:
+                journalText.text = "First entry added, three more to go";
+                break;
+            case 2:
+                journalText.text = journalText.text + System.Environment.NewLine + "Second entry added, two more to go";
+                break;
+            case 3:
+                journalText.text = journalText.text + System.Environment.NewLine + "Third entry added, one more to go";
+                break;
+            case 4:
+                journalText.text = journalText.text + System.Environment.NewLine + "Fourth entry added, now I can escape!";
+                break;
+            default:
+                journalText.text = "Nothing in here now";
+                break;
+        }
+    }
+}
--- Assets/Scripts/player/RaycastScript.cs
+++ Assets/Scripts/player/RaycastScript.cs
@@ -7,12 +7,23 @@ public class RaycastScript : MonoBehaviour
 {
     private GameObject raycastedObject;
 
-    [SerializeField] private int rayLength = 4;
-    [SerializeField] private LayerMask layerMaskInteract;
+    public GameObject gameManager;
 
+    //distance of interacting
+    [SerializeField] private readonly int rayLength = 4;
+
+    [SerializeField] private LayerMask layerMaskInteract;
     [SerializeField] private Image UICrosshair;
 
-    //private bool raycastHit = false;
+    private Text tooltipText;
+    private bool tooltipTextActive;
+
+    public bool raycastHit;
+
+    private void Start()
+    {
+        tooltipTextActive = false;
+    }
 
     void Update()
     {
@@ -24,23 +35,34 @@ public class RaycastScript : MonoBehaviour
             if(hit.collider.CompareTag("InteractableObject"))
             {
                 raycastedObject = hit.collider.gameObject;
+                //text object on the interactable object
+                tooltipText = raycastedObject.gameObject.GetComponentInChildren<Text>(true);
+                SetToolTipOn();
+
+                raycastHit = true;
+
                 //change the crosshair
                 CrosshairActive();
 
+                //interacting with the object
                 if(Input.GetKeyDown("e"))
                 {
                     Debug.Log("I have interacted");
                     raycastedObject.SetActive(false);
+                    gameManager.GetComponent<JournalProgression>().JournalAddText();
                 }
             }
         }
         else
         {
-            //crosshair to normal
+            //crosshair to normal when the ray hits nothaaaaaanggggg
+            raycastHit = false;
             CrosshairNormal();
+            SetToolTipOff();
         }
     }
 
+    //might want to use a sprite for this
     void CrosshairActive()
     {
         UICrosshair.color = Color.green;
@@ -49,5 +71,27 @@ public class RaycastScript : MonoBehaviour
     void CrosshairNormal()
     {
         UICrosshair.color = Color.white;
+        if(tooltipTextActive)
+        {
+            tooltipText.gameObject.SetActive(false);
+        }
+    }
+
+    void SetToolTipOn()
+    {
+        if(tooltipTextActive == false)
+        {
+            tooltipText.gameObject.SetActive(true);
+            tooltipTextActive = true;
+        }
+    }
+
+    void SetToolTipOff()
+    {
+        if (tooltipTextActive)
+        {
+            tooltipText.gameObject.SetActive(false);
+            tooltipTextActive = false;
+        }
     }
 }
