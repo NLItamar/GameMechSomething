be0716226b22d05f6a644902e8576713c0672986
Itamar <itamar@live.nl>
2020-04-08 18:38:19 +0200

    changes to enemy behaviour, setting up for lives lost and so on. currently decreases live int in gamemanager on every hit. so it goes ------------10000000000000

---
+++ Assets/Scripts/GameManager/LivingBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LivingBehaviour : MonoBehaviour
+{
+    public int currentLives;
+    private readonly int maxLives;
+
+    private void Update()
+    {
+        if(currentLives == 0)
+        {
+            Debug.Log("you lost");
+            //SceneManager.LoadScene("Level01");
+        }
+    }
+}
---
+++ Assets/Scripts/player/HitDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitDetection : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    private bool collidingEnemy;
+
+    private void Start()
+    {
+        collidingEnemy = false;
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.collider.tag == "Enemy" && collidingEnemy != false)
+        {
+            collidingEnemy = true;
+
+            gameManager.GetComponent<LivingBehaviour>().currentLives--;
+            Debug.Log(gameManager.GetComponent<LivingBehaviour>().currentLives);
+        }
+    }
+}
