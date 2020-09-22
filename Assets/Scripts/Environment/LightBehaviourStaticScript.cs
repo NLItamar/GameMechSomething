b56c2fac649a4814a49af0f80a63ff0e34c4b74e
Itamar <itamar@live.nl>
2020-09-22 15:48:05 +0200

    light flicker thingy almost done. It  flickers now but it should go off a tad longer. ( so out of the update and into invoke repeating or something else)

---
+++ Assets/Scripts/Environment/LightBehaviourStaticScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBehaviourStaticScript : MonoBehaviour
+{
+    Light myLight;
+
+    Material m_Material;
+
+    public float minIntensity = 0.3f;
+    public float maxIntensity = 2f;
+
+    public float maxCheck;
+    private float randomTime;
+    private float randomTimeChecker;
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+        randomTime = Random.Range(1f, maxCheck);
+        randomTimeChecker = maxCheck - 1f;
+        Debug.Log(randomTime);
+        Debug.Log(randomTimeChecker);
+
+        //InvokeRepeating("FlickerLight", 0f, 0.1f);
+
+        m_Material = GetComponent<Renderer>().material;
+
+    }
+
+    void Update()
+    {
+        //turns the light off
+        if (randomTime >= randomTimeChecker)
+        {
+            myLight.intensity = minIntensity;
+            m_Material.DisableKeyword("_EMISSION");
+        }
+
+        //turns the light on
+        if (randomTime <= randomTimeChecker)
+        {
+            myLight.intensity = maxIntensity;
+            m_Material.EnableKeyword("_EMISSION");
+        }
+
+        randomTime = Random.Range(1f, maxCheck);
+    }
+
+    private void FlickerLight()
+    {
+
+    }
+
+}
