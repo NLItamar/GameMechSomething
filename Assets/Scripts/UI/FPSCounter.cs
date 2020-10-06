a96c85de2c52eb29c414598feab3b95ffe780e1c
Itamar <itamar@live.nl>
2020-10-06 14:47:48 +0200

    lots of lights added. fps counter added. fog is now on far distance to remain the effect a bit. game settings added to lock framerate at 60 or local screen limit(vsync)

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int avgFrameRate;
+    public Text display_Text;
+
+    /*
+    public void Update()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = display_Text.text + avgFrameRate.ToString();
+    }
+    */
+
+    void Start()
+    {
+        InvokeRepeating("FPSTicker", 0f, 0.2f);
+    }
+
+    private void FPSTicker()
+    {
+        float current = 0;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = "FPS: " + avgFrameRate.ToString();
+    }
+}
