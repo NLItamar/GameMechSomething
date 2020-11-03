e317633c6fb626da2630472e307b4412ee15e8cc
Itamar <itamar@live.nl>
2020-11-03 18:23:38 +0100

    second intel now spawns more randomly located. made a method that can be used to spawn intels or other items randomly over positions from empty objects in a list of gameobjects. 5 mess locations(works) 30 SQ locations(not yet implemented)

--- Assets/Scripts/GameManager/StartLevelOneScript.cs
+++ Assets/Scripts/GameManager/StartLevelOneScript.cs
@@ -8,6 +8,14 @@ public class StartLevelOneScript : MonoBehaviour
     public GameObject RoofObject;
     public Text StartText;
 
+    public GameObject MessObject;
+
+    public GameObject MessSpawnPoints;
+
+    private List<GameObject> childrenList;
+
+    private int randomNumber;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +23,10 @@ public class StartLevelOneScript : MonoBehaviour
 
         //set active method
         SetObjectsActive();
+
+        //spawns the intels randomly
+        randomNumber = Random.Range(0, 5);
+        SpawnIntelLocationsRandom(randomNumber, MessSpawnPoints, MessObject);
     }
 
     void SetObjectsActive()
@@ -24,5 +36,27 @@ public class StartLevelOneScript : MonoBehaviour
 
         //set the start text active
         StartText.gameObject.SetActive(true);
+    }
+
+
+    //reusable method that moves an interactable object to another location
+    //needs a random number as much as the locations there are, which are the transform positions of the empty gameobjects
+    //needs the parent of those empty gameobjects and the interactable object itself
+    void SpawnIntelLocationsRandom(int randomSeed, GameObject randomSpawnPoints, GameObject toMoveObject)
+    {
+        childrenList = new List<GameObject>();
+
+        foreach(Transform child in randomSpawnPoints.transform)
+        {
+            childrenList.Add(child.gameObject);
+        }
+        //gives the location of the spot it'll move it to
+        Debug.Log(childrenList[randomSeed].transform.position);
+
+        //moves the interact object
+        toMoveObject.transform.position = childrenList[randomSeed].transform.position;
+
+        //clears the list for next object
+        childrenList.Clear();
     }
 }
